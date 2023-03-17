5f0af84f7351f6c093aea7e4505d0edacb8a187f
Drashti106 <dgdoshi.2003@gmail.com>
2023-03-18 02:21:19 +0530

    done

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,32 +3,75 @@ package com.driver;
 public class Pizza {
 
     private int price;
-    private Boolean isVeg;
     private String bill;
-
+    private int cheesePrice;
+    private int toppingPrice;
+    private int paperBegPrice;
+    private int totalPrice;
+    boolean extraCheese, extraTopping, paperBeg,billGen;
     public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
-        // your code goes here
+        if(isVeg){
+            price=300;
+            toppingPrice = 70;
+        }
+
+        else{
+            price+=400;
+            toppingPrice = 120;
+        }
+        bill = "Base Price Of The Pizza: 300 \n";
+        totalPrice = price;
+        cheesePrice = 80;
+        paperBegPrice = 20;
+        extraCheese = false;
+        extraTopping = false;
+        paperBeg = false;
+        billGen = false;
     }
 
     public int getPrice(){
-        return this.price;
+        return totalPrice;
     }
 
     public void addExtraCheese(){
         // your code goes here
+        if(!extraCheese){
+            totalPrice += cheesePrice;
+            extraCheese = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!extraTopping){
+            totalPrice += toppingPrice;
+            extraTopping = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!paperBeg){
+            totalPrice+=paperBegPrice;
+            paperBeg = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        if(!billGen){
+            if(extraCheese){
+                bill += "Extra Cheese Added: 80 \n";
+            }
+            if(extraTopping){
+                bill += "Extra Toppings Added: "+toppingPrice+"\n";
+            }
+            if(paperBeg){
+                bill += "Paperbag Added: 20 \n";
+            }
+            bill += "Total Price: " + totalPrice;
+            billGen = true;
+        }
+        return bill;
     }
 }
